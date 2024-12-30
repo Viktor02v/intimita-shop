7b9790145e77fb4a36b28a0e824d689aa8dc9d93
Viktor Petruk <damnnn261@gmail.com>
2024-12-30 13:04:23 +0200

    update (header, footer)

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,15 @@
 
 <template>
 
-	<section class="relative w-full h-[100vh]">
-  
-		<LayoutHeader/>
-    
-		<div class="w-full mt-[120px]">
-			<slot />
-		</div>
-    
-	</section>
+
+<section class="relative w-full h-[100vh]">
+    <LayoutHeader />
+    <div class="w-full mt-[80px]">
+      <slot />
+    </div>
+    <LayoutFooter />
+
+  </section>
   
 </template>
 <style scoped></style>
