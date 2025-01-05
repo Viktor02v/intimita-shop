fcddb51d703fba295c192e12bc29277d42162127
Viktor Petruk <damnnn261@gmail.com>
2025-01-05 10:01:26 +0200

    create a product page

---
+++ components/layout/product/ImageSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+defineProps({
+	img: {
+		type: String,
+		required: true
+	}
+})
+</script>
+
+<template>
+	<div
+		class="w-1/2 flex shadow-[0_4px_15px_rgba(0,0,0,0.1)] items-center border rounded justify-center min-h-[700px] max-h-[700px]">
+		<div class=" flex  items-center justify-center w-[600px] h-[600px]">
+			<NuxtImg :src="img" class="w-full h-full object-contain" alt="Product Image" />
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
--- composables/useGetProduct.ts
+++ composables/useGetProduct.ts
@@ -1,1 +1,13 @@
-import {useQuery} from "@tanstack/vue-query"
+import { useQuery } from "@tanstack/vue-query";
+import { DB_ID, COLLECTION_ITEMS } from "@/app.constants";
+import { DB } from "@/lib/appwrite";
+
+export function useGetProduct(productId:string) {
+return useQuery({
+	queryKey: ["product", productId ],
+	queryFn: async () => {
+		return DB.getDocument(DB_ID, COLLECTION_ITEMS, productId);
+	},
+	select: (data) => data,
+});
+}
