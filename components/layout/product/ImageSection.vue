e3bc19133777724e19a6f91dd790f9f6573e14df
Viktor Petruk <damnnn261@gmail.com>
2025-01-24 22:05:58 +0200

    adapt (product => [id].vue)

--- components/layout/product/ImageSection.vue
+++ components/layout/product/ImageSection.vue
@@ -10,9 +10,9 @@ defineProps({
 
 <template>
 	<div
-		class="w-1/2 flex shadow-[0_4px_15px_rgba(0,0,0,0.1)] items-center border rounded justify-center min-h-[700px] max-h-[700px]">
+		class="w-1/2 flex shadow-[0_4px_15px_rgba(0,0,0,0.1)] items-center border rounded justify-center min-h-[400px] md:min-h-[700px] max-h-[700px]">
 		<div class=" flex  items-center justify-center w-[600px] h-[600px]">
-			<NuxtImg :src="img" class="w-full h-full object-contain" alt="Product Image" />
+			<NuxtImg :src="img" class="w-[80%] md:w-full h-full object-contain" alt="Product Image" />
 		</div>
 	</div>
 </template>
