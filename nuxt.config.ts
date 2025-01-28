c138d683c7d1a043b79f14412ffff03a5fe4ef9c
Viktor02v <152213502+Viktor02v@users.noreply.github.com>
2025-01-28 16:58:05 +0200

    Revert "fix attempt #3"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,6 @@
 export default defineNuxtConfig({
 compatibilityDate: '2024-11-01',
 devtools: { enabled: true },
-postcss: {
-	plugins: {
-	  "postcss-simple-vars": {},
-	},
- },
 modules: [
   '@nuxtjs/tailwindcss',
   '@nuxt/image',
