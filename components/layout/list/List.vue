f627921a32df2988f78ed7f4307fa3fae68b0982
Viktor Petruk <damnnn261@gmail.com>
2025-01-30 15:19:07 +0200

    create hover effect

--- components/layout/list/List.vue
+++ components/layout/list/List.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
 	<div
-		class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4   w-full gap-6 md:gap-8 lg:gap-[50px] justify-items-center">
+		class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-6 md:gap-8 lg:gap-[50px] justify-items-center z-10">
 		<div v-for="item in items" :key="item.$id" class="flex justify-center">
 			<LayoutListCardList :item="item" />
 		</div>
